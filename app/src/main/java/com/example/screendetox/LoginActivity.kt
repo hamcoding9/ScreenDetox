c9b76873b24608b5f92db831076c4d47cbb9c3da
yxllout <dientes8520@gmail.com>
2022-10-01 21:12:44 +0900

    [환경 설정]DB 연동 [기능 추가]로그인 기능 추가

---
+++ app/src/main/java/com/example/screendetox/LoginActivity.kt
@@ -0,0 +1,76 @@
+package com.example.screendetox
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity:AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+
+        loginButton.setOnClickListener{
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful){
+                        finish()
+                    } else {
+                      Toast.makeText(
+                          this,
+                          "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요. ",
+                          Toast.LENGTH_SHORT
+                      ).show()
+                    }
+                }
+            }
+
+        emailEditText.addTextChangedListener{
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+
+        passwordEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+
+        signUpButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "회원가입을 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+        }
+    }
+}
--- app/src/main/java/com/example/screendetox/MainActivity.kt
+++ app/src/main/java/com/example/screendetox/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.screendetox
 
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
@@ -8,6 +11,11 @@ import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import java.util.stream.Collectors
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +23,9 @@ class MainActivity : AppCompatActivity() {
     var usersList: ListView? = null
     var enableBtn: Button? = null
     var permissionTv: TextView? = null
+
+    // 로그인 정보
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +37,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 어플을 처음 실행시켰을 때, permission 되어 있지 않으면 user permission setting 화면으로 넘어감
+    // 어플을 처음 실행시켰을 때, login 되어 있지 않으면 login 화면으로 넘어감
     override fun onStart()
     {
         super.onStart()
+        // login
+        if (auth.currentUser == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        // permission
         if(grantStatus){ // permission 허용되어 있으면
             showUsersList() // 유저 리스트 띄우기
         }
@@ -38,10 +56,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // 지난 24시간 동안의 유저들의 사용 시간을 보여줌.
+    // 지난 24시간 동안의 유저들의 사용 시간을 불러오는 함수 (List에 담기)
     fun loadStatistics()
     {
+        val usm = this.getSystemService(USAGE_STATS_SERVICE) as UsageStatsManager
 
+        // queryUsageStats(intervalType, beginTime, endTime) : MutableList<UsageStats!>!
+        // 지난 24시간 동안의 현재 사용자의 어플 사용 기록을 appList에 저장한다
+        var appList = usm.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY,
+            System.currentTimeMillis() - 1000 * 2600 * 24,
+            System.currentTimeMillis())
+
+        // 지난 24시간 동안 사용한 어플리케이션(사용 시간 > 0인 것만 filter)만 불러오기
+        // totalTimeInForeground: Get the total time this package spent in the foreground, measured in milliseconds.
+        appList = appList.stream().filter{ app: UsageStats -> app.totalTimeInForeground > 0}
+            .collect(Collectors.toList())
+        
+        // Group the usageStats by application and sort them by total time in foreground
+        if (appList.size > 0){
+            val mySortedMap: MutableMap<String, UsageStats> = TreeMap()
+            for (usageStats in appList) {
+                mySortedMap[usageStats.packageName] = usageStats
+            }
+            showAppsUsage(mySortedMap)
+        }
+    }
+    // 유저들의 사용 시간을 화면에 보이게 함
+    // View에 Data 전달
+
+    fun showAppUsage(mySortedMap: Map<String, UsageStats>){
+        val UsersList = ArrayList<User?>()
+        val usageStatsList: List<UsageStats> = ArrayList(mySortedMap.values) // UsageStats만 모아서 list
+
+        // 전체 시간 구하기
+        val totalTime = usageStatsList.stream().map {obj: UsageStats -> obj.totalTimeInForeground }
+            .mapToLong {obj: Long -> obj}.sum()
+
+        // UsersList 채우기
     }
 }
 
